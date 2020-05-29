e87463a2ce651e40e19d4c8498e41b36c6297e70
Birkan Çilingir <birkancilingir@hotmail.com>
2020-05-29 23:17:47 +0300

    Prometheus Configurations

--- src/main/java/me/brkn/tallyapi/core/configuration/WebSecurityConfiguration.java
+++ src/main/java/me/brkn/tallyapi/core/configuration/WebSecurityConfiguration.java
@@ -1,16 +1,13 @@
 package me.brkn.tallyapi.core.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Bean
-MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(){
-    return registry->registry.config().commonTags("application","MYAPPNAME");
-    }
-
+@Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   private final AuthenticationProvider authenticationProvider;
